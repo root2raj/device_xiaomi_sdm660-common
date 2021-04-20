fbdbd9e4a16a3ed088ed44df5e0b1e52901d0d8a
SakilMondal <mondalsakil@gmail.com>
2021-04-20 06:48:45 +0000

    Revert "sdm660-common: Switch to source built vendor.qti.hardware.perf@2.0.so"
    This reverts commit 18a3dc814cdddd8b4a89678dfb07a1fe7805d664.

--- sdm660.mk
+++ sdm660.mk
@@ -330,8 +330,6 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
-    vendor.qti.hardware.perf@2.0.vendor
-
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
