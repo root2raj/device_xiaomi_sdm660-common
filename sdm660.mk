2e21839f34dc86e9a347d5f136987f2e0453fe10
predator112 <mi560334@gmail.com>
2021-05-03 01:33:48 +0000

    fixlog error

--- sdm660.mk
+++ sdm660.mk
@@ -331,7 +331,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
+    vendor.qti.hardware.perf@2.0.vendor
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
