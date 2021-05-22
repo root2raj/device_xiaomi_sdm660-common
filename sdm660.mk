c02118b62655cfc884778f44a73371795c4d525b
predator <mi460334@agmail.com>
2021-05-22 12:53:57 +0000

    Revert "whyred: Add powerhint.json for sdm636"
    This reverts commit 5ac356cbc39c08f705043cf579cd41da516c8404.

--- sdm660.mk
+++ sdm660.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Powerhint
 ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
