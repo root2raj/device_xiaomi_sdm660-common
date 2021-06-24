7f7bec15ff08667b31d6c83954fe7429ed2dee10
iszon <wojtekst64@gmail.com>
2021-06-24 20:40:51 +0000

    sdm660-common: sepolicy: Address some denials

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@
 #rm -rf frameworks/base
 #rm -rf packages/apps/Settings
 #rm -rf frameworks/av#
-
+#rm -rf kernel/xiaomi/sdm660
 # git clone
 #git clone https://github.com/SakilMondal/hardware_qcom-caf_msm8998_display hardware/qcom-caf/msm8998/display
 #git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
@@ -14,4 +14,4 @@
 #git clone https://github.com/dotMod/android_packages_apps_Settings -b dot11 packages/apps/Settings
 #git clone https://github.com/dotMod/android_frameworks_av-1 -b dot11 frameworks/av
 #git clone https://github.com/11-whyred-new/MiuiCamera -b cr-9.0 vendor/MiuiCamera
-#git clone https://github.com/predator112/STOCK_Q_kernel_whyred -b android-10-eas kernel/xiaomi/sdm660
+#git clone https://github.com/predator112/android_kernel_xiaomi_sdm660_southwest -b 11.0-eas-wifi-r kernel/xiaomi/sdm660
