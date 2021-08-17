35d5a51a3ed8d51266b2dc470a989b720a72f096
root <root@predator.s23y2h5q3hpezf530mshdswirc.syx.internal.cloudapp.net>
2021-08-17 06:16:15 +0000

    fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,15 @@
 # rm -rf 
-#rm -rf hardware/qcom-caf/msm8998/display
-#rm -rf hardware/qcom-caf/msm8998/media
-#rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
 #rm -rf frameworks/base
 #rm -rf packages/apps/Settings
 #rm -rf frameworks/av#
 #rm -rf kernel/xiaomi/sdm660
 # git clone
-#git clone https://github.com/SakilMondal/hardware_qcom-caf_msm8998_display hardware/qcom-caf/msm8998/display
-#git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
-#git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/SakilMondal/hardware_qcom-caf_msm8998_display hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
 #git clone https://github.com/dotMod/android_frameworks_base -b dot11 frameworks/base
 #git clone https://github.com/dotMod/android_packages_apps_Settings -b dot11 packages/apps/Settings
 #git clone https://github.com/dotMod/android_frameworks_av-1 -b dot11 frameworks/av
